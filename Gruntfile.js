782c94c1c95e0d3f1e70fe3073981c4146e3b2a6
Chad Killingsworth <chad.killingsworth@banno.com>
2015-03-12 11:07:48 -0500

    Move final results to a 'packages' folder. Cleanup paths.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-string-replace');
@@ -11,12 +12,19 @@ module.exports = function (grunt) {
   });
 
   grunt.initConfig({
+    clean: {
+      compiler: ['packages/google-closure-compiler']
+    },
     copy: {
       compiler: {
         files: [
           {
-            src: ['closure-compiler/README.md', 'closure-compiler/COPYING'],
-            dest: 'packages/google-closure-compiler/'
+            src: ['closure-compiler/README.md'],
+            dest: 'packages/google-closure-compiler/README.md'
+          },
+          {
+            src: ['closure-compiler/COPYING'],
+            dest: 'packages/google-closure-compiler/COPYING'
           },
           {
             src: ['closure-compiler/target/closure-compiler-*-SNAPSHOT.jar'],
@@ -45,6 +53,6 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('compiler', ['cwd-closure-compiler', 'shell:mvn_compiler', 'cwd-parent',
-      'copy:compiler', 'string-replace:compiler']);
+  grunt.registerTask('compiler', ['clean:compiler', 'cwd-closure-compiler', 'shell:mvn_compiler',
+      'cwd-parent', 'copy:compiler', 'string-replace:compiler']);
 };
